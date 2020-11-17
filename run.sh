84efb40e56ed072bea551f747267b77aa16e06aa
adypd97 <adypd1997@gmail.com>
2020-11-17 07:40:42 +0530

    automation progress

--- run.sh
+++ run.sh
@@ -3,11 +3,21 @@
 # Run multiple simulations with various combinations of strategies, interaction
 # probabilities and iterations
 
-# For w = 0.99, n = 100
-echo -e "TFT vs TFT "
-./axelrod.py --s1=tft --s2=tft --show=True
-echo -e "\nAll D vs TFT"
-./axelrod.py --s1=all_D --s2=tft --show=True
-echo -e "\nAlternate vs TFT "
-./axelrod.py --s1=alt --s2=tft --show=True
+strategies=("tft" "all_D" "majority_seeker" "randomer" "alt" "trusting_fool")
 
+# For w = 0.99, n = 100
+#for choice_1 in "${strategies[@]}"; do
+#	for choice_2 in "${strategies[@]}"; do
+#		echo -e "\n ${choice_1}  vs ${choice_2}"
+#		./axelrod.py --s1="${choice_1}" --s2="${choice_2}" --n=100
+#	done
+#done
+
+# For w = 0.99, n = 200
+
+for choice_1 in "${strategies[@]}"; do
+	for choice_2 in "${strategies[@]}"; do
+		echo -e "\n ${choice_1}  vs ${choice_2}"
+		./axelrod.py --s1="${choice_1}" --s2="${choice_2}" --n=200
+	done
+done
